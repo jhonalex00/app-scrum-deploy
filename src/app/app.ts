d687d5655b478cfa43b2527de3a32de3fcec8baf
jhon alex caro <jcc24gr042@student.esgerencia.com>
2025-12-05 20:17:55 +0100

    feat: add demo component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+// Quitamos RouterOutlet porque no lo usamos
+import { DemoComponent } from './demo/demo';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [DemoComponent],   // Solo DemoComponent
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('app-scrum-deploy');
+  title = 'app-scrum-deploy';
 }
---
+++ src/app/demo/demo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  templateUrl: './demo.html',
+  styleUrl: './demo.css'
+})
+export class DemoComponent {}
